9ee942469bc9428ed87e1b955e17f6463f690b9c
Han Youngmin <youngmin.han@geappliances.com>
2025-03-31 16:31:12 +0900

    Refactor camera initialization tracking with _initializeFuture

--- packages/camera/camera/lib/src/camera_controller.dart
+++ packages/camera/camera/lib/src/camera_controller.dart
@@ -265,7 +265,11 @@ class CameraController extends ValueNotifier<CameraValue> {
 
   bool _isDisposed = false;
   StreamSubscription<CameraImageData>? _imageStreamSubscription;
-  FutureOr<bool>? _initCalled;
+
+  // A Future awaiting an attempt to initialize (e.g. after `initialize` was
+  // just called). If the controller has not been initialized at least once,
+  // this value is null.
+  Future<void>? _initializeFuture;
   StreamSubscription<DeviceOrientationChangedEvent>?
       _deviceOrientationSubscription;
 
@@ -294,11 +298,15 @@ class CameraController extends ValueNotifier<CameraValue> {
         'initialize was called on a disposed CameraController',
       );
     }
+
+    final Completer<void> initializeCompleter = Completer<void>();
+    _initializeFuture = initializeCompleter.future;
+
     try {
       final Completer<CameraInitializedEvent> initializeCompleter =
           Completer<CameraInitializedEvent>();
 
-      _deviceOrientationSubscription = CameraPlatform.instance
+      _deviceOrientationSubscription ??= CameraPlatform.instance
           .onDeviceOrientationChanged()
           .listen((DeviceOrientationChangedEvent event) {
         value = value.copyWith(
@@ -343,9 +351,9 @@ class CameraController extends ValueNotifier<CameraValue> {
       );
     } on PlatformException catch (e) {
       throw CameraException(e.code, e.message);
+    } finally {
+      initializeCompleter.complete();
     }
-
-    _initCalled = true;
   }
 
   /// Prepare the capture session for video recording.
@@ -365,7 +373,7 @@ class CameraController extends ValueNotifier<CameraValue> {
 
   /// Pauses the current camera preview
   Future<void> pausePreview() async {
-    if (value.isPreviewPaused) {
+    if (value.isPreviewPaused || !value.isInitialized || _isDisposed) {
       return;
     }
     try {
@@ -402,6 +410,11 @@ class CameraController extends ValueNotifier<CameraValue> {
       await CameraPlatform.instance.setDescriptionWhileRecording(description);
       value = value.copyWith(description: description);
     } else {
+      if (_initializeFuture != null) {
+        await _initializeFuture;
+        await CameraPlatform.instance.dispose(_cameraId);
+      }
+
       await _initializeWithDescription(description);
     }
   }
@@ -841,8 +854,8 @@ class CameraController extends ValueNotifier<CameraValue> {
     _unawaited(_deviceOrientationSubscription?.cancel());
     _isDisposed = true;
     super.dispose();
-    if (_initCalled != null) {
-      await _initCalled;
+    if (_initializeFuture != null) {
+      await _initializeFuture;
       await CameraPlatform.instance.dispose(_cameraId);
     }
   }
